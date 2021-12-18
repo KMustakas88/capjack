f9496e2c3177d7b6a0c97c24bd9e46d6e1dbbec9
Gus Mustakas <gmustakas@MacBook-Pro.local>
2021-12-18 10:49:15 -0500

    working example of django template view render

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('segments-detail/<str:pk>/', views.apiSegmentDetail, name='segments-detail'),
     path('segments-create/', views.apiSegmentCreate, name='segments-create'),
     path('segments-update/<str:pk>/', views.apiSegmentUpdate, name='segments-update'),
+
+    path('hello', views.hello, name='hello'),
 ]
