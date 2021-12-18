b26cc9bd6cde8d38d508a3af9ab46ec0e9698830
Gus Mustakas <gmustakas@MacBook-Pro.local>
2021-12-18 16:04:13 -0500

    simple App.js react app displaying segments table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+	state = {
+		segments: [],
+	}
+
+	componentDidMount() {
+
+		let data;
+
+		axios.get('http://localhost:8000/api/segments-list')
+			.then(res => {
+				data = res.data;
+				this.setState({
+					segments: data
+				});
+			})
+			.catch(err => { })
+	}
+
+	render() {
+		return (
+			<div>
+				{this.state.segments.map((segment) => (
+					// <div key={id}>
+					<div class="container">
+						<table class="table">
+
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Date Created</th>
+									<th>Last Updated</th>
+									<th>Combines</th>
+									<th>Totalable</th>
+									<th>Render</th>
+									<th>Customer</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>{ segment.name }</td>
+									<td>{ segment.created_at }</td>
+									<td>{ segment.updated_at }</td>
+									<td>{ segment.combines }</td>
+									<td>{ segment.totalable }</td>
+									<td>{ segment.render }</td>
+									<td>{ segment.customer }</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				)
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
